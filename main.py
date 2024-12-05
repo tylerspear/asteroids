174bb088cc6187f46d7108ed8d27e63e62210b73
Tyler Spear <tylerspear@Tylers-iMac.local>
2024-12-05 14:55:07 -0600

    initial asteroids commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+
+def main():
+    print("Starting asteroids!")
+
+#this ensures the main function only runs if the file is run directly, not as a module
+if __name__ == "__main__":
+    main()
