db554edcff003c8380bf18e6f82c441cde94d0a5
Tyler Spear <tylerspear@Tylers-iMac.local>
2024-12-05 21:44:08 -0600

    added screen

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from constants import *
 import pygame
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    
 #this ensures the main function only runs if the file is run directly, not as a module
 if __name__ == "__main__":
     main()
