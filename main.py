98edc3a533c7e8c3107b7e0edc65fd1ae09847e4
Tyler Spear <tylerspear@Tylers-iMac.local>
2024-12-07 16:34:49 -0600

    added rotation function

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    
-#this ensures the main function only runs if the file is run directly, not as a module
+        player.update(dt)
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
